15669e4b55348410ffa682adb606eeb65de08ad6
Eva-Mari <evamari@live.se>
2024-05-11 12:17:48 +0300

    emptying input field

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import { View, Text, Button, TextInput, Keyboard } from "react-native";
 import { Chips } from "../components/Chips";
-import WebView from "react-native-webview";
 import { Searchbar } from "react-native-paper";
 
 export function HomeScreen({ navigation }) {
@@ -9,28 +8,26 @@ export function HomeScreen({ navigation }) {
   const [url, setUrl] = React.useState("https://recept.se/sok"); //vad ska skickas till annan sida om flera hemsidor skrapas?
 
   useEffect(() => {
-    const encodedQuery = encodeURIComponent(searchQuery);
+    const encodedQuery = encodeURI(searchQuery);
     setUrl(`https://recept.se/sok?q=${encodedQuery}`);
-    //setSearchQuery(encodedQuery);
   }, [searchQuery]);
 
-  const handleSearch = (search) => {
-    //const encodedQuery = encodeURIComponent(search);
-    //setSearchQuery(encodedQuery);
-    //setUrl(`https://recept.se/sok?q=${encodedQuery}`);
-    //console.log("this is encoded query: '", encodedQuery, "'!");
-    console.log(search);
-    console.log(typeof serach);
-    console.log("this is the url: ", typeof url);
-    console.log("this is new url ", url);
-    navigation.navigate("SearchResultScreen", { url: url });
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("blur", () => {
+      setSearchQuery("");
+      Keyboard.dismiss();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
+  const handleSearch = () => {
+    navigation.navigate("SearchResultScreen", { url });
   };
 
   const changeQuery = (value) => {
     setUrl("https://recept.se/sok?" + value);
-    console.log("homescreen url: ", url);
+    navigation.navigate("SearchResultScreen", { url });
   };
-
   const data = [
     { label: "Bak & dessert", value: "tags=huvudkategori:1" },
     { label: "Dryck", value: "tags=huvudkategori:2" },
@@ -46,21 +43,15 @@ export function HomeScreen({ navigation }) {
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Searchbar
         placeholder="Sök recept"
-        onChangeText={handleSearch}
+        onChangeText={setSearchQuery}
         value={searchQuery}
+        onSubmitEditing={handleSearch}
       />
       <Chips data={data} changeQuery={changeQuery} />
       <Button
         onPress={() => navigation.navigate("SearchResultScreen", { url })}
         title="Sök"
       />
-      <TextInput
-        placeholder="Sök recept"
-        onChangeText={setSearchQuery}
-        value={searchQuery}
-        style={{ borderWidth: 1, padding: 10, marginBottom: 10, width: 300 }}
-      />
-      <Button onPress={() => handleSearch(searchQuery)} title="Testa nya sök" />
     </View>
   );
 }
