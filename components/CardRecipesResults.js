ed80599206504994cbee9fd55b638d53a9b4a1b8
Eva-Mari <evamari@live.se>
2024-05-11 12:15:59 +0300

    Added component for the second data source

--- components/CardRecipesResults.js
+++ components/CardRecipesResults.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Card, Text, Button } from "react-native-paper";
 import { StarRatingDisplay } from "react-native-star-rating-widget";
+import { Image, View } from "react-native";
 
 export const SearchResultCard = ({ recipe, retrieveSlugValue }) => {
   const handlePress = () => {
     retrieveSlugValue(recipe.slug);
   };
+
   return (
     <Card>
       <Card.Title title={recipe.media.description} />
@@ -17,5 +19,30 @@ export const SearchResultCard = ({ recipe, retrieveSlugValue }) => {
         <Button onPress={handlePress}>Gå till recept</Button>
       </Card.Actions>
     </Card>
+  );
+};
+
+export const ReceptenResultCard = ({ recipe, retrieveLink }) => {
+  const handlePress = () => {
+    retrieveLink(recipe.slug);
+  };
+  return (
+    <View>
+      <Image
+        source={{
+          uri: recipe.image,
+        }}
+      />
+      <Card>
+        <Card.Title title={recipe.title}></Card.Title>
+        <Card.Cover source={{ uri: recipe.image }} />
+        <Card.Content>
+          <Text>{recipe.description}</Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button onPress={handlePress}>Gå till recept</Button>
+        </Card.Actions>
+      </Card>
+    </View>
   );
 };
