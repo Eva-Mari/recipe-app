4723639092d5cb3161941320a769d8b98556eebc
Eva-Mari <evamari@live.se>
2024-05-12 22:41:27 +0300

    Changed to stack navigation

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { HomeScreen } from "./screens/HomeScreen";
 import { RecipesScreen } from "./screens/RecipesScreen";
 import { SearchResultScreen } from "./screens/SearchResultScreen";
 
-const Drawer = createDrawerNavigator();
+//const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
           name="SearchResultScreen"
           component={SearchResultScreen}
         />
-        <Drawer.Screen name="Recipes" component={RecipesScreen} />
-      </Drawer.Navigator>
+        <Stack.Screen name="Recipes" component={RecipesScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
